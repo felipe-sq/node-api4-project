ae053ecbb3dafbd823339019809b3f2f003a9b52
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-08-12 16:49:32 -0400

    Working on MVP tasks. Npm dependencies have been installed and a simple API has been created.

---
+++ index.js
@@ -0,0 +1,54 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const server = express();
+const port = process.env.PORT || 9000;
+
+server.use(cors());
+server.use(express.json());
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+
+server.get('/', (req, res) => {
+  res.status(200).json('Hello World!');
+});
+
+server.get('/api/users', (req, res) => {
+  res.status(200).json([
+    {
+      id: 1,
+      name: 'John Doe',
+      email: "john@email.com"
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      email: "jane@email.com"
+    }
+  ]);
+});
+
+server.post('/api/register', (req, res) => {
+  const { username, password } = req.body; 
+  if(!username || !password) {
+    res.status(400).json({ message: 'Name and email are required' });
+  } else {
+    res.status(200).json({
+    message: 'User created successfully!'
+    });
+  }
+});
+
+server.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  if(!username || !password) {
+    res.status(400).json({ message: 'Email and password are required' });
+  } else {
+    res.status(200).json({
+      message: 'User logged in successfully!'
+    });
+  }
+});
